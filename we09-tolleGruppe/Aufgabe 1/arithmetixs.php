bca3f1232137c09de65ebe9dc1158ff61bc6f210
GruseliX <spannagel.marcel@gmail.com>
2019-12-18 17:44:39 +0100

    we09 angefangen und Aufgabe 1 fertig

---
+++ we09-tolleGruppe/Aufgabe 1/arithmetixs.php
@@ -0,0 +1,48 @@
+<?php 
+
+$input = "− / 15 − 7 + 1 1 3 + 2 + 1 1";
+$functionTable = array("+" => "add","-" => "sub", "*" => "mult", "/" => "div");
+$preprocessed = array("a","b");
+
+// Funktion zur zwei Parameter Addition 
+function add($a,$b) {
+    return $a + $b;
+}
+
+// Funktion zur zwei Parameter Subtraktion
+function sub($a,$b) {
+    return $a - $b;
+}
+
+// Funktion zur zwei Parameter Multiplikation 
+function mult($a,$b) {
+    return $a * $b;
+}
+
+// Funktion zur zwei Parameter Division 
+function div($a,$b) {
+    return $a / $b;
+}
+
+function inProcessed(&$input, &$functionTable, &$preprocessed){
+    $temp = explode(" ",$input);
+    $temp1 = array_intersect_key($temp,$functionTable);
+    $temp = array_replace($temp, $temp1);
+    $preprocessed = getArray($temp);
+    echo "hallo";
+
+}
+
+function getArray($array) {
+    return $array;
+}
+
+
+// echo add(1,2); //3
+// echo sub(4,3); //1
+// echo mult(4,2); //8
+// echo div(10,2); //5
+// echo "<br>";
+inProcessed($input,$functionTable,$preprocessed);
+print_r($preprocessed);
+?>
