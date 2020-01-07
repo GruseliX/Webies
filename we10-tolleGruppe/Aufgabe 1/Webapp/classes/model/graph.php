9bf2892daae560083e8d6c6c4fe571cd3201d149
GruseliX <spannagel.marcel@gmail.com>
2020-01-07 19:41:57 +0100

    Aufgabe 1 graph.php bearbeitet

--- we10-tolleGruppe/Aufgabe 1/Webapp/classes/model/graph.php
+++ we10-tolleGruppe/Aufgabe 1/Webapp/classes/model/graph.php
@@ -6,15 +6,32 @@ class Graph{
     
     function __construct(){}
 
-
+    /**
+     * addNode
+     * Adds a specific Node to the graph.
+     * @param id the ID of the new node 
+     */
     function addNode($id){
-        this.$nodes = array_push($nodes,$id);
+        if ($nodes[$id] == null){
+            this.$nodes = array_push($nodes,$id);
+        }
+        else{
+            throw new Exception("the Node with the ID ".$id." exists alredy." );
+        }
     }
 
-    function addEdge($startId, $endid, $cost, $line){
+    /**
+     * addEdge
+     * Adds a specific Edge between tow nodtes to the graph.
+     * @param startId   : id of the startnode
+     * @param endId     : id of the endnode
+     * @param cost      : the cost of this new edge
+     * @param line      : the line that drives on this edge
+     */
+    function addEdge($startId, $endId, $cost, $line){
         $startNode = this.findNode($startId);
         $endNode = this.findNode($endId);
-        $newEdge = new Edge($endnode, $cost, $line);
+        $newEdge = new Edge($endNode, $cost, $line);
         if($startNode == null){
             throw new Exception($startId." is not an startnode" );
         }
@@ -25,22 +42,47 @@ class Graph{
         this.$notes.array_push($newEdge);
     }
 
+    /**
+     * findNode
+     * Serches for a specific node in the graph
+     * @param id the ID of the searched node 
+     */
     function findNode($id){
         $node = null;
         $node = nodes[$id];
         if($node == null){
             throw new Exception("A node with that id ". $id . "is not contained in this graph." ); 
+            return;
         }
         return node;
     }
 
+    /**
+     * print
+     * Prints the praph with all its nodes and their edges  
+     */
     function print(){
-        //TODO fertigstellen
+        echo "<p>"
+        foreach($notes as $val){
+            $edges = $val.getEdges();
+            
+            echo $val." ";
+            foreach($edges as $v){
+                echo "-> "
+                echo $v.getEndnode().getId();
+            }
+            echo "<br>";
+        }
+        echo "</p>";
     }
 
+    /**
+     * getGraph
+     * Returns the whole graph 
+     * @return this [Graph] returns the whole graph 
+     */
     function getGraph(){
-        //TODO fertigstellen
-        return edge;
+        return this;
     }
 }
 ?>
